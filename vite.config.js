de62603e7dbef3a4ce42ff8bf8c11def34efd953
Sylvie Trolle <sylvie.trl22@gmail.com>
2026-01-21 15:52:05 +0100

    Suppression imports SCSS obsolètes et configuration Vite pour l’injection globale des variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables" as *;\n`,
+      },
+    },
+  },
+});
